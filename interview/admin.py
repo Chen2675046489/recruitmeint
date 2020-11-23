3f27001a0ced3398fce345bb3bdd18f5b5e113ac
CWJ <2675046489@qq.co>
2020-11-23 19:22:36 +0800

    '更新代码'

--- interview/admin.py
+++ interview/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from interview.models import Candidate
 # Register your models here.
+
+
+class CandidateAdmin(admin.ModelAdmin):
+    exclude = ('creator', 'creator_date', 'modified_date')
+
+    list_display = (
+        'username', 'city', 'bachelor_school', 'first_score', 'first_result', 'first_interviewer',
+        'second_result', 'second_interviewer', 'hr_score', 'hr_result', 'last_editor'
+    )
+    fieldsets = (
+        (None, {'fields': ("userid", "username",)}),
+        ('第一轮面试记录', {'fields': ("",)}),
+        ('第二轮面试记录', {'fields': ("",)}),
+        ('HR终面记录', {'fields': ("",)}),
+    )
+
+admin.site.register(Candidate, CandidateAdmin)
--- interview/models.py
+++ interview/models.py
@@ -41,3 +41,58 @@ class Candidate(models.Model):
     first_professional_competency = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=2,
                                                         verbose_name="专业能力得分")
 
+    first_advantage = models.TextField(max_length=1024, blank=True, verbose_name='优势')
+    first_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name='顾虑和不足')
+    first_result = models.CharField(max_length=256, choices=INTERVIEW_RESULT_TYPE, blank=True,
+                                    verbose_name='初试结果')
+    first_recommend_position = models.CharField(max_length=256, blank=True, verbose_name='推荐部分')
+    first_interviewer = models.CharField(max_length=1024, blank=True, verbose_name='面试官')
+    first_remark = models.CharField(max_length=256, blank=True, verbose_name='初试备注')
+
+    # 第二轮面试
+    second_score = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True, verbose_name='专业复试得分')
+    second_learning_ability = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
+                                                  verbose_name='学习能力得分')
+    second_professional_competency = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
+                                                         verbose_name='追求卓越得分')
+    second_pursue_of_excellence = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
+                                                      verbose_name='沟通能力得分')
+    second_pressure_score = models.DecimalField(decimal_places=1, null=True, max_digits=2, blank=True,
+                                                verbose_name='抗压能力得分')
+    second_advantage = models.TextField(max_length=1024, blank=True, verbose_name='优势')
+    second_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name='顾虑和不足')
+    second_result = models.CharField(max_length=256, choices=FIRST_INTERVIEW_RESULT_TYPE, blank=True,
+                                     verbose_name='专业f复试结果')
+    second_recommend_position = models.CharField(max_length=256, blank=True, verbose_name='建议方向或推荐部门')
+    second_interviewer = models.CharField(max_length=256, blank=True, verbose_name='面试官')
+    second_remark = models.CharField(max_length=256, blank=True, verbose_name='专业复试备注')
+
+    # HR终面
+    hr_score = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name='HR复试综合等级')
+    hr_responsibility = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name='HR责任心')
+    hr_communication_ability = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True,
+                                                verbose_name='坦诚沟通')
+    hr_logic_ability = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name='HR逻辑思维')
+    hr_potential = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name='HR发展能力')
+    hr_stability = models.CharField(max_length=10, choices=HR_SCORE_TYPE, blank=True, verbose_name='HR稳定性')
+    hr_advantage = models.TextField(max_length=1024, blank=True, verbose_name='优势')
+    hr_disadvantage = models.TextField(max_length=1024, blank=True, verbose_name='顾虑与不足')
+    hr_result = models.CharField(max_length=256, choices=INTERVIEW_RESULT_TYPE, blank=True, verbose_name='HR复试结果')
+    hr_interviewer = models.CharField(max_length=1024, blank=True, verbose_name='HR面试官')
+    hr_remark = models.CharField(max_length=256, blank=True, verbose_name='HR面试备注')
+
+    creator = models.CharField(max_length=256, blank=True, verbose_name='候选人数据的创建人')
+    creator_date = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    modified_date = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name='更新时间')
+    last_editor = models.CharField(max_length=256, blank=True, verbose_name='最后编辑人')
+
+    class Meta:
+        db_table = 'candidate'
+        verbose_name = '应聘者'
+        verbose_name_plural = '应聘者'
+
+    def __unicode__(self):
+        return self.username
+
+    def __str__(self):
+        return self.username
