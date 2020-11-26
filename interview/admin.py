b350a44b53b6a325ca2bc32b52dfcd4182417c99
CWJ <2675046489@qq.co>
2020-11-26 16:04:02 +0800

    提交新得代码

--- interview/admin.py
+++ interview/admin.py
@@ -1,11 +1,49 @@
 from django.contrib import admin
 from interview.models import Candidate
+from django.http import HttpResponse
 # Register your models here.
+import csv
+from datetime import datetime
+import logging
+logger = logging.getLogger(__name__)
+
+exportable_fields = ('username', 'city', 'phone', 'bachelor_school', 'master_school', 'doctor_school',
+                     'first_result', 'first_interviewer', 'second_result', 'second_interviewer', 'hr_score', 'hr_result'
+                     )
+
+
+def export_model_as_excel(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    fields_list = exportable_fields
+    response['Content-Disposition'] = 'attachment; filename=exportable-list-%s.csv' \
+                                      % (datetime.now().strftime('%y-%m-%d-%H-%M-%S'),
+                                         )
+    # 编写表头
+    writer = csv.writer(response)
+    writer.writerow(
+        [queryset.model._meta.get_field(f).verbose_name for f in fields_list]
+    )
+
+    for obj in queryset:
+        csv_line_values = []
+        for field in fields_list:
+            field_object = queryset.model._meta.get_field(field)
+            fields_value = field_object.value_from_object(obj)
+            csv_line_values.append(fields_value)
+        writer.writerow(csv_line_values)
+        logger.info('%s 导出 %s 条应聘者资料' %(request.user, len(queryset)))
+    return response
+
+
+# 可以定义函数得属性为中文名称
+export_model_as_excel.short_description = '导出为CSV文件'
 
 
 # 候选人管理类
 class CandidateAdmin(admin.ModelAdmin):
     exclude = ('creator', 'creator_date', 'modified_date')
+
+    actions = [export_model_as_excel, ]
 
     list_display = (
         'username', 'city', 'bachelor_school', 'first_score', 'first_result', 'first_interviewer',
---
+++ settings/local.py
@@ -0,0 +1,9 @@
+from .base import *
+
+ALLOWED_HOSTS = ["recruit.ihopeit.com", "127.0.0.1"]
+
+DEBUG = True
+
+INSTALLED_APPS = (
+    
+)
